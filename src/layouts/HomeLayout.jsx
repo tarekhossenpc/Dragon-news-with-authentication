97cc4c878c7914bf5d484537b157b2041cc5047b
Tarek Hossen <tarekhossenpc@gmail.com>
2026-05-14 16:02:41 +0600

    github pushing experiment

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -19,7 +19,7 @@ const HomeLayout = () => {
         </nav>
       </header>
       <main className="w-11/12 mx-auto my-3  grid grid-cols-12 gap-5">
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <LeftAside></LeftAside>
         </aside>
         <section className="main col-span-6">
